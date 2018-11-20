19451d8e2b9e1628f75d587ec90b093ca57fb35c
Jonathan Omarwoth <oma0256@gmail.com>
2018-11-20 17:39:44 +0300

    Add custom http library

---
+++ UI/static/js/http.js
@@ -0,0 +1,74 @@
+// Class for a custom http
+class Http {
+  // Method to retrieve headers
+  getHeaders() {
+    const token = localStorage.getItem("token");
+    const headers = { "Content-Type": "application/json" };
+    if (token) {
+      headers["Authorization"] = "Bearer " + token;
+    }
+    return headers;
+  }
+
+  // Perform get call to external api
+  async get(url) {
+    const headers = this.getHeaders();
+    const res = await fetch(url, {
+      method: "GET",
+      headers: headers
+    });
+    const resData = await res.json();
+    const output = {
+      status: res.status,
+      data: resData
+    };
+    return output;
+  }
+
+  // Post data to external api
+  async post(url, data) {
+    const headers = this.getHeaders();
+    const res = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data)
+    });
+    const resData = await res.json();
+    const output = {
+      status: res.status,
+      data: resData
+    };
+    return output;
+  }
+
+  // Modify data in external api
+  async put(url, data) {
+    const headers = this.getHeaders();
+    const res = await fetch(url, {
+      method: "PUT",
+      headers: headers,
+      body: JSON.stringify(data)
+    });
+    const resData = await res.json();
+    const output = {
+      status: res.status,
+      data: resData
+    };
+    return output;
+  }
+
+  // Delete data from external api
+  async delete(url) {
+    const headers = this.getHeaders();
+    const res = await fetch(url, {
+      method: "DELETE",
+      headers: headers
+    });
+    const resData = await res.json();
+    const output = {
+      status: res.status,
+      data: resData
+    };
+    return output;
+  }
+}
