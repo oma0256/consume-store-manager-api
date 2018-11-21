71d9020b633ff65d373443297d62e838fb6aaf27
Jonathan Omarwoth <oma0256@gmail.com>
2018-11-21 17:47:19 +0300

    Add js to handle retrieving and display of a single product

---
+++ UI/static/js/product-detail.js
@@ -0,0 +1,48 @@
+const http = new Http();
+// Get product id
+const productId = localStorage.getItem("productId");
+const isAdmin = localStorage.getItem("isAdmin");
+let output;
+
+// Make request to get a single product
+http
+  .get(
+    `https://oma-store-manager-api.herokuapp.com/api/v2/products/${productId}`
+  )
+  .then(res => {
+    // Check if product was fetched
+    if (res.status === 200) {
+      product = res.data.product;
+      if (isAdmin == "true") {
+        output = `<div id="desc-side"><p>Name: ${product.name}</p><p>Price: ${
+          product.unit_cost
+        }</p><p>Quantity: ${
+          product.quantity
+        }</p></div><div id="btn-side"><button id="edit-btn" class="modal-display">Edit</button><a><button class="delete-btn">Delete</button></a></div>`;
+        if (product.category) {
+          output = "";
+          output = `<div id="desc-side"><p>Name: ${product.name}</p><p>Price: ${
+            product.unit_cost
+          }</p><p>Quantity: ${product.quantity}</p><p>Category: ${
+            product.category
+          }</p></div><div id="btn-side"><button id="edit-btn" class="modal-display">Edit</button><a><button class="delete-btn">Delete</button></a></div>`;
+        }
+      } else {
+        output = `<div id="desc-side"><p>Name: ${product.name}</p><p>Price: ${
+          product.unit_cost
+        }</p><p>Quantity: ${
+          product.quantity
+        }</p></div><div id="btn-side"><button id="add-to-cart">Add to Cart</button></div>`;
+        if (product.category) {
+          output = "";
+          output = `<div id="desc-side"><p>Name: ${product.name}</p><p>Price: ${
+            product.unit_cost
+          }</p><p>Quantity: ${product.quantity}</p><p>Category: ${
+            product.category
+          }</p></div><div id="btn-side"><button id="add-to-cart">Add to Cart</button><a></div>`;
+        }
+      }
+      const productArea = document.querySelector("#product-detail");
+      productArea.innerHTML = output;
+    }
+  });
