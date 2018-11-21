1d4f2462958e169930889001ac04ee25255ee4ec
Jonathan Omarwoth <oma0256@gmail.com>
2018-11-21 17:14:01 +0300

    Handle js to fetch products from api

---
+++ UI/static/js/products.js
@@ -0,0 +1,47 @@
+const http = new Http();
+const isAdmin = localStorage.getItem("isAdmin");
+
+// Make a request to get all products
+http
+  .get("https://oma-store-manager-api.herokuapp.com/api/v2/products")
+  .then(res => {
+    // Check if request was successful
+    if (res.status === 200) {
+      const products = res.data.products;
+      const productsArea = document.querySelector(".products");
+      let output = "";
+      // Check if there are products
+      if (products.length < 1) {
+        output += "<h2 class='center-head'>There are no products yet</h2>";
+      } else {
+        products.forEach(product => {
+          if (isAdmin == "true") {
+            output += `<div class="product"><div class="product-desc"><h2>${
+              product.name
+            }</h2><h3>Price: UGX ${
+              product.unit_cost
+            }</h3></div><a href="product-detail.html"><button class="product-detail">Details</button></a><input type="hidden" value=${
+              product.id
+            } id="product-id"></div>`;
+          } else {
+            output += `<div class="product"><div class="product-desc"><h2>${
+              product.name
+            }</h2><h3>Price: UGX ${
+              product.unit_cost
+            }</h3></div><a href="cart.html"><button class="add-to-cart">Add to Cart</button></a><a href="product-detail.html"><button class="product-detail">Details</button></a><input type="hidden" value=${
+              product.id
+            } id="product-id"></div>`;
+          }
+        });
+      }
+      // Add products to web page
+      productsArea.innerHTML = output;
+      // Run if products weren't fetched
+    } else {
+      if (isAdmin == "true") {
+        window.location = "http://127.0.0.1:5500/UI/admin/login.html";
+      } else {
+        window.location = "http://127.0.0.1:5500/UI/attendant/login.html";
+      }
+    }
+  });
