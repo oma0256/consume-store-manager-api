a89d5973c92affd9876b3bebd97b49cca6ce9a59
Jonathan Omarwoth <oma0256@gmail.com>
2018-11-21 18:56:19 +0300

    Add js to handle edit product

--- UI/static/js/product-detail.js
+++ UI/static/js/product-detail.js
@@ -3,6 +3,7 @@ const http = new Http();
 const productId = localStorage.getItem("productId");
 const isAdmin = localStorage.getItem("isAdmin");
 let output;
+let varPro;
 
 // Make request to get a single product
 http
@@ -44,6 +45,7 @@ http
       }
       const productArea = document.querySelector("#product-detail");
       productArea.innerHTML = output;
+      varPro = product;
     } else {
       if (isAdmin == "true") {
         window.location = "http://127.0.0.1:5500/UI/admin/login.html";
@@ -52,3 +54,55 @@ http
       }
     }
   });
+
+const editDeleteProduct = e => {
+  const editForm = document.querySelector("#product-edit");
+  if (e.target.className == "modal-display") {
+    if (varPro.category) {
+      output = `<label>Name</label><br><input type="text" id="product-name" value=${
+        varPro.name
+      }><label>Price</label><br><input type="number" id="product-price" value=${
+        varPro.unit_cost
+      }><label>Quantity</label><br><input type="number" id="product-quantity" value=${
+        varPro.quantity
+      }><label>Category</label><br><input type="text" id="product-category" value=${
+        varPro.category
+      }><button type="submit" class="modal-btn">Submit</button>`;
+    }
+    output = `<label>Name</label><br><input type="text" id="product-name" value=${
+      varPro.name
+    }><label>Price</label><br><input type="number" id="product-price" value=${
+      varPro.unit_cost
+    }><label>Quantity</label><br><input type="number" id="product-quantity" value=${
+      varPro.quantity
+    }><label>Category</label><br><input type="text" id="product-category" value="Select a Category"><button type="submit" class="modal-btn">Submit</button>`;
+    editForm.innerHTML = output;
+  }
+  if (e.target.className == "modal-btn") {
+    e.preventDefault();
+    const productName = document.querySelector("#product-name").value;
+    const productPrice = document.querySelector("#product-price").value;
+    const productQuantity = document.querySelector("#product-quantity").value;
+    const productCategory = document.querySelector("#product-category").value;
+    const productData = {
+      name: productName,
+      unit_cost: parseInt(productPrice),
+      quantity: parseInt(productQuantity)
+    };
+    if (productCategory != "Select a Category") {
+      productData["category"] = productCategory;
+    }
+    http
+      .put(
+        `https://oma-store-manager-api.herokuapp.com/api/v2/products/${productId}`,
+        productData
+      )
+      .then(function(res) {
+        if (res.status == 200) {
+          editForm.submit();
+        }
+      });
+  }
+};
+
+window.addEventListener("click", editDeleteProduct);
