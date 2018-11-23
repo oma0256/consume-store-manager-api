e894aefffca5ce2c99682297c5793bf92e3e1fec
Jonathan Omarwoth <oma0256@gmail.com>
2018-11-23 19:28:34 +0300

    Modify sales.js logic to match api

--- UI/static/js/sales.js
+++ UI/static/js/sales.js
@@ -11,19 +11,12 @@ http
       } else {
         output += `<h1 class="center-head">Sales Records</h1><table class="table"><tr><th>Product</th><th>Quantity</th><th>Amount</th></tr>`;
         saleRecords.forEach(sale => {
-          http
-            .get(
-              `https://oma-store-manager-api.herokuapp.com/api/v2/products/${
-                sale.product_id
-              }`
-            )
-            .then(res => {
-              output += `<tr><td>${res.data.product.name}</td><td>${
-                sale.quantity
-              }</td><td>${sale.total}</td></tr>`;
-              salesArea.innerHTML = output;
-            });
+          output += `<tr><td>${sale.product_name}</td><td>${
+            sale.quantity
+          }</td><td>${sale.total}</td></tr>`;
         });
+        output += `</table>`;
+        salesArea.innerHTML = output;
       }
     }
   });
